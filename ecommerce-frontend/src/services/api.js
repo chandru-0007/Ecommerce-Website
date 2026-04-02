c6911f760013c3b9c6afc241f042db3b48a07895
Chandru S <chandru042007@gmail.com>
2026-04-02 10:32:58 +0530

    Fix FRONTend

--- ecommerce-frontend/src/services/api.js
+++ ecommerce-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8081/api';
+const API_BASE_URL = 'https://ecommerce-website-2i22.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
